6dff408844ce003b0937f7bdbda3e8ce239004c6
Nelson Londoño <nelsonlondonodev@gmail.com>
2026-05-16 13:19:56 +0200

    fix(ui): ensure version is correctly imported from package.json in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+import { version } from './package.json'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -22,7 +23,7 @@ export default defineConfig(({ mode }) => {
 
   return {
     define: {
-      'import.meta.env.VITE_APP_VERSION': JSON.stringify(process.env.npm_package_version || '1.0.0'),
+      'import.meta.env.VITE_APP_VERSION': JSON.stringify(version),
     },
     plugins: [react(), tailwindcss()],
     resolve: {
